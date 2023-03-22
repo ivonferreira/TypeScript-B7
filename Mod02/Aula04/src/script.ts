febba87508a8fb2df07b6d42515680d16ca9bc35
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-03-22 15:14:16 -0300

    Aulas de TS B7

---
+++ Mod02/Aula04/src/script.ts
@@ -0,0 +1,4 @@
+
+let infos: any = ['Ivon','Jose','Marcos','Claudia',38,1.8]
+
+infos.push(true);
---
+++ Mod02/Aula06/src/script.ts
@@ -0,0 +1,4 @@
+function somar(n1: number,n2:number): number{
+    return n1 + n2
+}
+let algo = somar(95,5)
